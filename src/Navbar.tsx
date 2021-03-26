bc0787e4aba5521a607e7b2b3e6ccb8bb80c8227
Karissa <phillips.karissa00@gmail.com>
2021-03-26 13:51:56 -0400

    mobile navbar styles

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,15 +1,52 @@
 import * as React from 'react';
 import styles from './Navbar.module.css';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
+import close from './images/close.svg';
+import menu from './images/menu.svg';
 
 const Navbar = () => {
-    return(
+    const history = useHistory();
+    const [openMenu, setOpenMenu] = React.useState<boolean>(false);
+    const onToggleMenu = () => {
+        setOpenMenu(!openMenu);
+    }
+    let currentPage = "";
+    if (history.location.pathname === "/projects") {
+        currentPage = "Projects";
+    } else if (history.location.pathname === "/bio") {
+        currentPage = "Bio";
+    } else if (history.location.pathname === "/skills") {
+        currentPage = "Skills";
+    } else if (history.location.pathname === "/contact") {
+        currentPage = "Contact";
+    }
+    return (
         <div className={styles.container}>
-            <NavLink to="/home" activeClassName={styles.selected}>Home</NavLink>
-            <NavLink to="/projects" activeClassName={styles.selected}>Projects</NavLink>
-            <NavLink to="/bio" activeClassName={styles.selected}>Bio</NavLink>
-            <NavLink to="/skills" activeClassName={styles.selected}>Skills</NavLink>
-            <NavLink to="/contact" activeClassName={styles.selected}>Contact</NavLink>
+            <div className={styles.header}>
+                <button onClick={onToggleMenu}><img src={menu} alt="menu" /></button>
+                <div>{currentPage}</div>
+            </div>
+            {
+                openMenu ?
+                    <div className={styles['mobile-menu']}>
+                        <button onClick={onToggleMenu}><img src={close} alt="close" /></button>
+                        <div>
+                            <NavLink to="/home" activeClassName={styles.selected}>Home</NavLink>
+                            <NavLink to="/projects" activeClassName={styles.selected}>Projects</NavLink>
+                            <NavLink to="/bio" activeClassName={styles.selected}>Bio</NavLink>
+                            <NavLink to="/skills" activeClassName={styles.selected}>Skills</NavLink>
+                            <NavLink to="/contact" activeClassName={styles.selected}>Contact</NavLink>
+                        </div>
+                    </div> : null
+            }
+            <div className={styles['desktop-menu']}>
+                            <NavLink to="/home" activeClassName={styles.selected}>Home</NavLink>
+                            <NavLink to="/projects" activeClassName={styles.selected}>Projects</NavLink>
+                            <NavLink to="/bio" activeClassName={styles.selected}>Bio</NavLink>
+                            <NavLink to="/skills" activeClassName={styles.selected}>Skills</NavLink>
+                            <NavLink to="/contact" activeClassName={styles.selected}>Contact</NavLink>
+
+            </div>
         </div>
     )
 }
