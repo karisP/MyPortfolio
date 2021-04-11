f5091fed2f6f61831b7c86c51cf77a68a9e316bf
Karissa <phillips.karissa00@gmail.com>
2021-04-11 19:34:36 -0400

    final home design

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react';
 import styles from './Home.module.css';
-// import profile from '../../images/profilepic.jpg';
 
 const Home = () => {
     return(
         <div className={styles.container}>
-            <div><span>This</span><span>Is</span><span>Me</span></div>
+            <div className={styles.name}>
+                <span>code + design</span>
+                <span>PORTFOLIO</span>
+                <span>Karissa Phillips</span></div>
             <div>
                 <div className={styles.image}></div>
-                <div className={styles.name}><span>Karissa</span><span>Phillips</span></div>
             </div>
         </div>
     )
