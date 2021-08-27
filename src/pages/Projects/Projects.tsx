2869e225d3ec9e611593d0b4300d4850d9cfe1e1
Karissa <phillips.karissa00@gmail.com>
2021-08-27 10:19:28 -0400

    updates to content

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -12,11 +12,12 @@ const Projects = () => {
             <div>
                 <p>For School</p>
                 <a className={styles.planimal} href="https://planimal-app.herokuapp.com/#!/home" target="blank"><div>Made with Angular, Node.js, Express</div></a>
+                <a className={styles.avengers} href="https://avengers-budget-app.herokuapp.com/" target="blank"><div>Made with jQuery</div></a>
             </div>
             <div>
                 <p>For Fun</p>
                 <a className={styles.guesshoot} href="https://guess-hoot.herokuapp.com/" target="blank"><div>Made with React and Typescript</div></a>
-                <a className={styles.apple} href="https://apple-doppelganger.herokuapp.com/" target="blank"><div>NEW! Current work in progress!</div></a>
+                <a className={styles.apple} href="https://apple-doppelganger.herokuapp.com/" target="blank"><div>Made with React and Typescript</div></a>
             </div>
             </div>
         </div>
