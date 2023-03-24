1182cf28ccf9926d52bbd06561b152c23df2b4ff
Karissa <phillips.karissa00@gmail.com>
2023-03-24 15:00:55 -0400

    added new projects

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -8,12 +8,13 @@ const Projects = () => {
                 <p>For Work</p>
                 <a className={styles.acd} href="http://acdmail.com/" target="blank"><div>Made with React</div></a>
                 <a className={styles.brushwatson} href="http://brushwatson.com/" target="blank"><div>Made with React and .NET</div></a>
-                {/* <a className={styles.youthup} href="https://dpcyouthup.org/" target="blank"><div>Made with React and Typescript</div></a> */}
+                <a className={styles.youthup} href="https://dpcyouthup.org/" target="blank"><div>Made with React, Typescript, and Material UI</div></a>
             </div>
             <div>
                 <p>For Fun</p>
                 <a className={styles.guesshoot} href="https://guess-hoot.herokuapp.com/" target="blank"><div>Made with React and Typescript</div></a>
                 <a className={styles.apple} href="https://apple-doppelganger.herokuapp.com/" target="blank"><div>Made with React and Typescript</div></a>
+                <a className={styles.pomodoro} href="https://pompomodoro.herokuapp.com/" target="blank"><div>Made with React & Reddit API</div></a>
             </div>
             </div>
         </div>
