4059813edfb1f66fa6b576da9142681e2c79bb81
Karissa <phillips.karissa00@gmail.com>
2023-07-27 13:28:53 -0400

    initial style changes

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,17 +1,18 @@
-import styles from './Home.module.css';
+import styles from "./Home.module.css";
 
 const Home = () => {
-    return(
-        <div className={styles.container}>
-            <div className={styles.name}>
-                <span>code + design</span>
-                <span>PORTFOLIO</span>
-                <span>Karissa Phillips</span></div>
-            <div>
-                <div className={styles.image}></div>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className={styles.container}>
+      <div className={styles.name}>
+        <span>Hi, I'm</span>
+        <span>Karissa Phillips</span>
+        <span>Frontend Engineer & Mentor</span>
+      </div>
+      <div>
+        <div className={styles.image}></div>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
