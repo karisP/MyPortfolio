3569d790fea62adb93d4d85836adad0ca0dc9db2
Karissa <phillips.karissa00@gmail.com>
2023-07-28 12:27:36 -0400

    images set and layout

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -2,14 +2,19 @@ import { ProjectData } from "../../pages/Projects/data";
 import styles from "./Project.module.css";
 
 const Project = ({ title, image, link, skills }: ProjectData) => {
+  const divStyle = {
+    backgroundImage: "url(" + image + ")",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  };
   return (
-    <div className={styles.container}>
-      <div className={styles.image} style={{ backgroundImage: `${image}` }} />
+    <a className={styles.container} href={link}>
+      <div className={styles.image} style={divStyle} />
       <div>
         <p>{title}</p>
         <div className={styles.skills}></div>
       </div>
-    </div>
+    </a>
   );
 };
 
