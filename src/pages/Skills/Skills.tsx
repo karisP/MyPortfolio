5c3b434527ab74968a3877d3120bb090117fbd78
Karissa <phillips.karissa00@gmail.com>
2023-07-28 15:56:42 -0400

    mobile portrait styles

--- src/pages/Skills/Skills.tsx
+++ src/pages/Skills/Skills.tsx
@@ -1,42 +1,20 @@
-import styles from './Skills.module.css';
+import SkillPill from "../../components/SkillPill/SkillPill";
+import styles from "./Skills.module.css";
+import skillsData from "./data";
+import vector from "../../images/triangle_vectors.svg";
 
 const Skills = () => {
-    return (
-        <div className={styles.container}>
-            <div>
-                <div>
-                    <div>Work Experience</div>
-                    <div>
-                        <span>*React.js</span>
-                        <span>*Typescript</span>
-                        <span>*Sass</span>
-                        <span>*GraphQL</span>
-                        <span>*.NET</span>
-                        <span>*Figma Design</span>
-                        <span>*Visual Studio</span>
-                        <span>*React Material UI</span>
-                        <span>*React Testing Library</span>
-                        <span>*Storybook</span>
-                        <span>*Swagger</span>
-                        <span>*Agile Development</span>
-                    </div>
-                </div>
-                <div>
-                    <div>School Experience</div>
-                    <div>
-                        <span>*Angular</span>
-                        <span>*Javascript</span>
-                        <span>*HTML</span>
-                        <span>*CSS</span>
-                        <span>*Node</span>
-                        <span>*Express</span>
-                        <span>*SQL</span>
-                        <span>*Github</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        {skillsData.map((s) => (
+          <SkillPill text={s} />
+        ))}
+        <img src={vector} className={styles.topVector} alt="decoration" />
+        <img src={vector} className={styles.bottomVector} alt="decoration" />
+      </div>
+    </div>
+  );
+};
 
 export default Skills;
---
+++ src/pages/Skills/data.ts
@@ -0,0 +1,22 @@
+const skillsData = [
+  "HTML",
+  "JavaScript",
+  "CSS",
+  "React",
+  "TypeScript",
+  "SCSS",
+  "GraphQL",
+  "REST API",
+  "SQL",
+  ".NET",
+  "Angular",
+  "MaterialUI",
+  "Storybook",
+  "Figma",
+  "Unit Testing",
+  "Swagger",
+  "Agile",
+  "Jira",
+];
+
+export default skillsData;
