df176ecec6a768ccf25d607988ea21827c1f9796
Karissa <phillips.karissa00@gmail.com>
2023-07-28 16:57:33 -0400

    update routes and remove bio

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import './App.css';
-import { Switch, Route } from 'react-router-dom';
-import Home from './pages/Home/Home';
-import Projects from './pages/Projects/Projects';
-import Bio from './pages/Bio/Bio';
-import Contact from './pages/Contact/Contact';
-import Skills from './pages/Skills/Skills';
-import Navbar from './Navbar';
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Projects from "./pages/Projects/Projects";
+import Contact from "./pages/Contact/Contact";
+import Skills from "./pages/Skills/Skills";
+import Navbar from "./Navbar";
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
       <Switch>
-        <Route exact path='/' render={() => <Home />}/>
-        <Route exact path='/projects' render={() => <Projects />} />
-        <Route exact path='/bio' render={() => <Bio />} />
-        <Route exact path='/contact' render={() => <Contact />} />
-        <Route exact path='/skills' render={() => <Skills />} />
+        <Route exact path="/" render={() => <Home />} />
+        <Route exact path="/projects" render={() => <Projects />} />
+        <Route exact path="/about" render={() => <Contact />} />
+        <Route exact path="/skills" render={() => <Skills />} />
       </Switch>
     </div>
   );
-}
+};
 
 export default App;
--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,58 +1,89 @@
-import * as React from 'react';
-import styles from './Navbar.module.css';
-import { NavLink, useHistory } from 'react-router-dom';
-import close from './images/close.svg';
-import menu from './images/menu.svg';
+import * as React from "react";
+import styles from "./Navbar.module.css";
+import { NavLink, useHistory } from "react-router-dom";
+import close from "./images/close.svg";
+import menu from "./images/menu.svg";
 
 const Navbar = () => {
-    const history = useHistory();
-    const [openMenu, setOpenMenu] = React.useState<boolean>(false);
+  const history = useHistory();
+  const [openMenu, setOpenMenu] = React.useState<boolean>(false);
 
-    const onToggleMenu = () => {
-        setOpenMenu(!openMenu)   
-    }
+  const onToggleMenu = () => {
+    setOpenMenu(!openMenu);
+  };
 
-    let currentPage = "";
-    if (history.location.pathname === "/projects") {
-        currentPage = "Projects";
-    } else if (history.location.pathname === "/bio") {
-        currentPage = "Bio";
-    } else if (history.location.pathname === "/skills") {
-        currentPage = "Skills";
-    } else if (history.location.pathname === "/contact") {
-        currentPage = "Contact";
-    }
-    console.log(openMenu, history);
+  let currentPage = "";
+  if (history.location.pathname === "/projects") {
+    currentPage = "Projects";
+  } else if (history.location.pathname === "/skills") {
+    currentPage = "Skills";
+  } else if (history.location.pathname === "/about") {
+    currentPage = "About";
+  }
+  console.log(openMenu, history);
 
-    return (
-        <div className={styles.container}>
-            <div className={styles.header}>
-                <button onClick={onToggleMenu}><img src={menu} alt="menu" /></button>
-                <div>{currentPage}</div>
-            </div>
-            {
-                openMenu ?
-                    <div className={styles['mobile-menu']}>
-                        <button onClick={onToggleMenu}><img src={close} alt="close" /></button>
-                        <div>
-                            <NavLink exact to="/" activeClassName={styles.selected} onClick={() => onToggleMenu()}>Home</NavLink>
-                            <NavLink to="/projects" activeClassName={styles.selected} onClick={() => onToggleMenu()}>Projects</NavLink>
-                            <NavLink to="/bio" activeClassName={styles.selected} onClick={() => onToggleMenu()}>Bio</NavLink>
-                            <NavLink to="/skills" activeClassName={styles.selected} onClick={() => onToggleMenu()}>Skills</NavLink>
-                            <NavLink to="/contact" activeClassName={styles.selected} onClick={() => onToggleMenu()}>Contact</NavLink>
-                        </div>
-                    </div> : null
-            }
-            <div className={styles['desktop-menu']}>
-                            <NavLink exact to="/" activeClassName={styles.selected}>Home</NavLink>
-                            <NavLink to="/projects" activeClassName={styles.selected}>Projects</NavLink>
-                            <NavLink to="/bio" activeClassName={styles.selected}>Bio</NavLink>
-                            <NavLink to="/skills" activeClassName={styles.selected}>Skills</NavLink>
-                            <NavLink to="/contact" activeClassName={styles.selected}>Contact</NavLink>
-
-            </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <button onClick={onToggleMenu}>
+          <img src={menu} alt="menu" />
+        </button>
+        <div>{currentPage}</div>
+      </div>
+      {openMenu ? (
+        <div className={styles["mobile-menu"]}>
+          <button onClick={onToggleMenu}>
+            <img src={close} alt="close" />
+          </button>
+          <div>
+            <NavLink
+              exact
+              to="/"
+              activeClassName={styles.selected}
+              onClick={() => onToggleMenu()}
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/projects"
+              activeClassName={styles.selected}
+              onClick={() => onToggleMenu()}
+            >
+              Projects
+            </NavLink>
+            <NavLink
+              to="/skills"
+              activeClassName={styles.selected}
+              onClick={() => onToggleMenu()}
+            >
+              Skills
+            </NavLink>
+            <NavLink
+              to="/about"
+              activeClassName={styles.selected}
+              onClick={() => onToggleMenu()}
+            >
+              About
+            </NavLink>
+          </div>
         </div>
-    )
-}
+      ) : null}
+      <div className={styles["desktop-menu"]}>
+        <NavLink exact to="/" activeClassName={styles.selected}>
+          Home
+        </NavLink>
+        <NavLink to="/projects" activeClassName={styles.selected}>
+          Projects
+        </NavLink>
+        <NavLink to="/skills" activeClassName={styles.selected}>
+          Skills
+        </NavLink>
+        <NavLink to="/about" activeClassName={styles.selected}>
+          About
+        </NavLink>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
