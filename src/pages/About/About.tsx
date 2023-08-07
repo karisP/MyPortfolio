64fe0aebf0dc660752625c8abe69dbefdda3fcf2
Karissa <phillips.karissa00@gmail.com>
2023-08-07 18:00:23 -0400

    movile headers and links

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,10 +1,11 @@
 import Card from "../../components/Card/Card";
 import styles from "./About.module.css";
+import { ReactComponent as Vector } from "../../images/triangle_vectors.svg";
 
 const About = () => {
   return (
     <div className={styles.container}>
-      <section>
+      <section className={styles.values}>
         <h1>People-First Approach</h1>
         <div className={styles.cardWrapper}>
           <Card
@@ -19,6 +20,34 @@ const About = () => {
             title="Team Focus"
             text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "
           />
+        </div>
+      </section>
+      <section className={styles.timeline}>
+        <div className={styles.header}>
+          <div className={styles.circle} />
+          <h1>Timeline</h1>
+        </div>
+      </section>
+      <section className={styles.connect}>
+        <div className={styles.header}>
+          <Vector />
+          <h1>Follow & Connect</h1>
+          <Vector />
+        </div>
+        <div className={styles.links}>
+          <a href="https://www.linkedin.com/in/karissaphillips" target="_blank">
+            LinkedIn
+          </a>
+          <span>&#124;</span>
+          <a href="https://github.com/karisP" target="_blank">
+            GitHub
+          </a>
+          <span>&#124;</span>
+          <a href="https://theprecisedev.com/" target="_blank">
+            Blog
+          </a>
+          <span>&#124;</span>
+          <a href="mailto: kpdevelopment313@gmail.com">Email</a>
         </div>
       </section>
     </div>
