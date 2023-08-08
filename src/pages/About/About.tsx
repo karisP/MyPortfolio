c0b8506389aa6db515b3c73f2767cc26dfc7d07f
Karissa <phillips.karissa00@gmail.com>
2023-08-08 13:01:41 -0400

    final additions

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -11,15 +11,15 @@ const About = () => {
         <div className={styles.cardWrapper}>
           <Card
             title="User Focus"
-            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "
+            text="I am passionate about delivering intuitive user interfaces that exceed expectations. My skills in implementing pixel-perfect designs have earned me a reputation for delivering high-quality work. Having been trained in WCAG, I advocate for accessibilty because all users deserve a great experience."
           />
           <Card
             title="Client Focus"
-            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "
+            text="I prioritize client satisfaction and excel at tailoring presentations to ensure clear communication of project progress and team achievements to both technical and non-technical audiences. I have had great experiences working with client designers and quality engineers."
           />
           <Card
             title="Team Focus"
-            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "
+            text="I advocate for cross-role communication between designers, quality engineers, delivery leads, and software teams. I believe that every person involved in the software development cycle is vital to the product's success, and I am always looking for opportunities to collaborate with others to create exceptional user experiences. "
           />
         </div>
       </section>
